45d54b511b3f10d0adf9f43e2cdc4905c1417c20
sudho712 <sudho712@gmail.com>
2024-10-02 12:28:29 +0530

    print no of sequence

---
+++ Pattern/seq_no_print.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=5;
+    int num=1;
+     for(int i=0; i<n; i++)
+     {
+        for(int j=0; j<n; j++)
+        {
+            cout<< num <<" ";
+            num++;
+        }
+        cout<<endl;
+     }
+    return 0;
+}
