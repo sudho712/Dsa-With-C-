f511d29a99fe807fd604b2e37bc504281bb15c50
sudho712 <sudho712@gmail.com>
2024-10-02 12:36:16 +0530

    seq character print

--- Pattern/char_seq_print.cpp
+++ Pattern/char_seq_print.cpp
@@ -2,14 +2,15 @@
 using namespace std;
 int main()
 {
-    int n=3;
-    int num=1;
+    
+    int n=5;
+    char ch='A';
      for(int i=0; i<n; i++)
      {
         for(int j=0; j<n; j++)
         {
-            cout<< num <<" ";
-            num++;
+            cout<< ch <<" ";
+            ch=ch+1;
         }
         cout<<endl;
      }
