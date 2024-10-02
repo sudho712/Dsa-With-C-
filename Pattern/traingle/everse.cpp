b6b32a2e25351d1f9a08c6c6ecf289ba554ee089
sudho712 <sudho712@gmail.com>
2024-10-02 12:54:50 +0530

    print reverse print

--- Pattern/traingle/everse.cpp
+++ Pattern/traingle/everse.cpp
@@ -5,7 +5,11 @@ int main()
     int n=5;
     for(int i=5; i>0; i--)
     {
-        cout<<i <<endl;
+        for(int j=i+1; j>0; j--)
+        {
+            cout<< j<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
